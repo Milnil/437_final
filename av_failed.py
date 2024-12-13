4b32350507fec025d0b9c4bb61dd7655601a98d8
PNPTCMilind <m.philar@pnptc.com>
2024-12-12 18:28:09 -0600

    new server2.py

--- av_failed.py
+++ av_failed.py
@@ -23,6 +23,7 @@ class MJPEGHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=frame')
             self.end_headers()
+            logging.info(f"WOOOOOOOOOOO")
 
             while True:
                 if not self.server.running:
